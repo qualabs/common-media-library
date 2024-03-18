f93ca3905cf354c9907f346e76daaec225eb53cb
Fernando Cuadro <fernandoc@qualabs.com>
2024-03-18 15:28:56 -0300

    updated response on call content steering server function
    Signed-off-by: Fernando Cuadro <fernandoc@qualabs.com>

--- lib/src/contentSteering/callContentSteeringServer.ts
+++ lib/src/contentSteering/callContentSteeringServer.ts
@@ -1,12 +1,31 @@
+interface ContentSteeringServerResponse {
+	status: number;
+	data: string;
+	headers: Record<string, string>;
+	url: string;
+}
+
 export async function callContentSteeringServer(
 	serverURI: string | null
-): Promise<string> {
+): Promise<ContentSteeringServerResponse> {
 	if (!serverURI) {
 		throw new Error('Server URI is null');
 	}
+
 	const response = await fetch(serverURI);
-	if (!response.ok) {
-		throw new Error(`HTTP error! status: ${response.status}`);
-	}
-	return response.text();
+	const data = await response.text();
+
+	const headers: Record<string, string> = {};
+	response.headers.forEach((value, key) => {
+		headers[key] = value;
+	});
+
+	const ContentSteeringServerResponse: ContentSteeringServerResponse = {
+		status: response.status,
+		data: data,
+		headers: headers,
+		url: response.url,
+	};
+
+	return ContentSteeringServerResponse;
 }

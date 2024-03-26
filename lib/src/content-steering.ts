4f365aff593ecfdf4dfe6b8cfc9c48891a5187e0
Fernando Cuadro <fernandoc@qualabs.com>
2024-03-26 14:48:46 -0300

    removed export of unused function
    Signed-off-by: Fernando Cuadro <fernandoc@qualabs.com>

--- lib/src/content-steering.ts
+++ lib/src/content-steering.ts
@@ -1,2 +1,1 @@
-export { parseM3u8ContentSteeringURI } from './contentsteering/parseM3u8ContentSteeringURI.js';
 export { callContentSteeringServer } from './contentsteering/callContentSteeringServer.js';

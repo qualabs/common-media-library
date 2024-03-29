653bd838ba6108b5adbff94695908ea63122f705
Fernando Cuadro <fernandoc@qualabs.com>
2024-03-29 14:26:34 -0300

    updated export for content steering
    Signed-off-by: Fernando Cuadro <fernandoc@qualabs.com>

--- lib/src/contentSteering.ts
+++ lib/src/contentSteering.ts
@@ -1,3 +1,3 @@
 export type { PathwayClone } from './contentsteering/PathwayClone.js';
-export type { SteeringManifest } from './contentsteering/SteeringManifest.js';
+export type { ContentSteeringResponse } from './contentsteering/SteeringManifest.js';
 export type { UriReplacement } from './contentsteering/UriReplacement.js';

69e81a24c7bc5ab3717c7e37c5cbe4fb5cb6433c
Fernando Cuadro <fernandoc@qualabs.com>
2024-03-29 14:20:07 -0300

    changed content steering respone to class
    Signed-off-by: Fernando Cuadro <fernandoc@qualabs.com>

--- lib/src/contentsteering/SteeringManifest.ts
+++ lib/src/contentsteering/SteeringManifest.ts
@@ -1,9 +1,15 @@
-import { PathwayClone } from './PathwayClone';
+export class ContentSteeringResponse {
+	version: number | null;
+	ttl: number;
+	reloadUri: string | null;
+	pathwayPriority: string[];
+	pathwayClones: any;
 
-export type SteeringManifest = {
-  VERSION: 1;
-  TTL: 300;
-  'RELOAD-URI'?: string;
-  'PATHWAY-PRIORITY': string[];
-  'PATHWAY-CLONES'?: PathwayClone[];
-};
+	constructor(response?: any) {
+		this.version = response.VERSION ?? null;
+		this.ttl = response.TTL ?? 300;
+		this.reloadUri = response?.['RELOAD-URI'] ?? null;
+		this.pathwayPriority = response?.['PATHWAY-PRIORITY'] ?? null;
+		this.pathwayClones = response?.['PATHWAY-CLONES'] ?? [];
+	}
+}
